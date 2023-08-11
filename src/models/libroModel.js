e571f55423b8d4169e78da6f8ce0516e4f94f4d3
maxels <maximascietti@gmail.com>
2023-08-11 01:38:14 -0300

    Test unitario y cambio en libroModel

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect("mongodb://127.0.0.1:27017/biblioteca", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
